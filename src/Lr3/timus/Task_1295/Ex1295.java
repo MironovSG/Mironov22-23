70f662e9fb2ad8ca6dfab1cc458738df36292a3e
sergey <xy2vdd@gmail.com>
2023-01-08 19:43:02 +0500

    Lr 2

---
+++ src/Lr3/timus/Task_1295/Ex1295.java
@@ -0,0 +1,27 @@
+package Lr3.timus.Task_1295;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class Ex1295 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n;
+        int zeroCount = 1;
+        do {
+            n = in.nextInt();
+        }
+        while (n < 1 || n > 300000);
+        if (n%4==0) zeroCount = 0;
+        else
+        if ((n/10)%2 == 0){
+            if (n%10==3 || n%10==5 || n%10==7)
+                zeroCount = 2;
+        }
+        else if (n%10==1 || n%10==5 || n%10==9)
+            zeroCount = 2;
+        out.println(zeroCount);
+
+        out.flush();
+        in.close();
+    }
+}
---
+++ src/Lr3/timus/Task_1296/Ex1296.java
@@ -0,0 +1,29 @@
+package Lr3.timus.Task_1296;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class Ex1296 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n;
+        int value;
+        int pot = 0;
+        int maxPot = 0;
+        do {
+            n = in.nextInt();
+        }
+        while (n < 0 || n > 60000);
+        for (int i = 0; i < n; i++) {
+            do {
+                value = in.nextInt();
+            }
+            while (Math.abs(value) > 30000);
+            pot += value;
+            if(pot < 0) pot = 0;
+            if(pot > maxPot) maxPot = pot;
+            }
+            out.println(maxPot);
+            out.flush();
+            in.close();
+        }
+    }
